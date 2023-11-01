f0b9ffcce4c9852ab2d46324a1fe6ce45a89a552
Gordon Schroeder <gordon.schroeder@alchemisten.ag>
2023-11-01 17:54:46 +0100

    refactor: Throw error if useTranslationsInfo() is used with TranslationsProvider
    BREAKING CHANGE: useTranslationsInfo() no longer works outside TranslationsProvider

--- src/hooks/translations.ts
+++ src/hooks/translations.ts
@@ -1,10 +1,15 @@
 import { useIntl } from 'react-intl';
-import { useMemo } from 'react';
-import { useTranslations } from '../provider';
+import { useContext, useMemo } from 'react';
+import { TranslationsContext } from '../provider';
 
 export const useTranslationsInfo = (languageNamePrefix = '__lang.', languageNameKey = '__lang.__name') => {
-  const { currentLanguage: languageKey, setLanguage, translations } = useTranslations();
   const intl = useIntl();
+  const context = useContext(TranslationsContext);
+  if (!context) {
+    throw new Error('useTranslationsInfo must be used within a TranslationsProvider');
+  }
+
+  const { currentLanguage: languageKey, setLanguage, translations } = context;
 
   const { languageName, currentLanguage } = useMemo(() => {
     return {
